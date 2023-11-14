dce5ffbce9bffd403123d92de77896f51a6ce5ed
Tamerlan Imanov <44167972+arch1tect1@users.noreply.github.com>
2023-11-14 22:24:50 +0400

    first commit

---
+++ Hash Tables/Item_In_Common.py
@@ -0,0 +1,15 @@
+# Write a function item_in_common(list1, list2) that takes two lists as input and returns True if there is at least one common item between the two lists, False otherwise.
+def item_in_common(list1, list2):
+    my_dict = {}
+    for i in list1:
+        my_dict[i] = True
+    for j in list2:
+        if j in my_dict:
+            return True
+    return False
+
+list1 = [1,3,5]
+list2 = [2,4,5]
+
+
+print(item_in_common(list1, list2))
