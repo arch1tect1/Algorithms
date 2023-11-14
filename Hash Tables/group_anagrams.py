982decfe4efa49501a6a21f6991cff81bbd44dcd
Tamerlan Imanov <44167972+arch1tect1@users.noreply.github.com>
2023-11-14 22:30:14 +0400

    first commit

---
+++ Hash Tables/group_anagrams.py
@@ -0,0 +1,19 @@
+# You have been given an array of strings, where each string may contain only lowercase English letters. You need to write a function group_anagrams(strings) that groups the anagrams in the array together using a hash table (dictionary). The function should return a list of lists, where each inner list contains a group of anagrams.
+
+# For example, if the input array is ["eat", "tea", "tan", "ate", "nat", "bat"], the function should return [["eat","tea","ate"],["tan","nat"],["bat"]] because the first three strings are anagrams of each other, the next two strings are anagrams of each other, and the last string has no anagrams in the input array.
+
+# You need to implement the group_anagrams(strings) function and return a list of lists, where each inner list contains a group of anagrams according to the above requirements.
+
+
+
+
+
+def group_anagrams(strings):
+    anagram_groups = {}
+    for string in strings:
+        canonical = ''.join(sorted(string))
+        if canonical in anagram_groups:
+            anagram_groups[canonical].append(string)
+        else:
+            anagram_groups[canonical] = [string]
+    return list(anagram_groups.values())
